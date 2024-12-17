01f3d618f7ae70cbbc2a4fc1e8bf4d97bdae984f
Leonardo Lopes <leobo215@hotmail.com>
2024-12-17 20:54:09 -0300

    try-catch & bcrypt
    Fiz algumas modificações, foi adicionado try-catch para tratamento de erro e o bcrypt para criptografar a senha do usuário

--- routes/public.js
+++ routes/public.js
@@ -1,23 +1,30 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
 
 const prisma = new PrismaClient()
 const router = express.Router()
 
 //Cadastro
 router.post('/cadastro', async (req, res) => {
+    
+    try {
+
     const user = req.body
-
-
-    await prisma.user.create({
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(user.password, salt)
+    const userDB = await prisma.user.create({
         data: {
             email: user.email,
             name: user.name,
-            password
-        }
+            password: hashPassword,
+        },
     })
 
-    res.status(201).json(user)
+    res.status(201).json(userDB)
+} catch (err) {
+    res.status(500).json({ message: 'Erro no Servidor, tente novamente' })
+}
 })
 
 export default router
