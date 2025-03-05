164e1e49b259087f9c9888377982bf87bfb98fbc
Leonardo Lopes <leobo215@hotmail.com>
2025-03-04 21:40:01 -0300

    middlewares
    neste commit, foi criado um middlewares para quando for feita uma requisição no auth pelo usuário, ele prosseguir para próxima página ou não.

---
+++ middlewares/auth.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken'
+
+const JWT_SECRET = process.env.JWT_SECRET
+
+const auth = (req, res, next) => {
+   
+    const token = req.headers.authorization
+
+    console.log(token)
+
+    if(!token) {
+        return res.status(401).json({ message: 'Acesso Negado'})
+    }
+
+    try {
+        const decoded = jwt.verify(token.replace('Bearer ', ''), JWT_SECRET)
+
+        console.log(decoded)
+
+    }catch(err){
+        return res.status(401).json({ message: 'Token Inválido'})
+    }
+
+
+    next()
+}
+
+export default auth
