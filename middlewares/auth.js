23fd71b6036843cf8640ea225f294ccd091cc029
Leonardo Lopes <leobo215@hotmail.com>
2025-03-06 21:51:43 -0300

    a

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,26 +3,23 @@ import jwt from 'jsonwebtoken'
 const JWT_SECRET = process.env.JWT_SECRET
 
 const auth = (req, res, next) => {
-   
+
     const token = req.headers.authorization
 
-    console.log(token)
 
-    if(!token) {
-        return res.status(401).json({ message: 'Acesso Negado'})
+    if (!token) {
+        return res.status(401).json({ message: 'Acesso Negado' })
     }
 
     try {
         const decoded = jwt.verify(token.replace('Bearer ', ''), JWT_SECRET)
-
         req.userId = decoded.id
+        next()
+    } catch (err) {
+        return res.status(401).json({ message: 'Token Inválido' })
 
-    }catch(err){
-        return res.status(401).json({ message: 'Token Inválido'})
     }
 
-
-    next()
 }
 
 export default auth
--- server.js
+++ server.js
@@ -13,3 +13,5 @@ app.use('/', auth, privateRoutes)
 
 
 app.listen(3000, () => console.log("Servidor Rodando"))
+
+
