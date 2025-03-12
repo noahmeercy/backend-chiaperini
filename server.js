90042c0789496508840bb1d8bf6645a047856d94
Leonardo Lopes <leobo215@hotmail.com>
2025-03-12 02:05:37 -0300

    UPDATE ROTAS PRIVADAS
    ATUALIZEI A ORGANIZAÇÃO DAS ROTAS PRIVADAS

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import publicRoutes from './routes/public.js'
 import privateRoutes from './routes/private.js'
+import fichaRoutes from './routes/CadastroFicha.js'
 import cors from 'cors'
 import auth from './middlewares/auth.js'
 
@@ -8,7 +9,13 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+
+// 🔓 Rotas públicas
 app.use('/', publicRoutes)
-app.use('/',auth , privateRoutes)
+
+
+// 🔒 Rotas privadas protegidas
+app.use('/private', auth, privateRoutes)
+app.use('/private', auth, fichaRoutes)
 
 app.listen(3000, () => console.log("Servidor Rodando"))
