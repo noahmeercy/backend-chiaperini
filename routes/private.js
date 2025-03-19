37f264c85a830be6d12f88a299bd00064ed8dd53
Leonardo Lopes <leobo215@hotmail.com>
2025-03-19 01:36:25 -0300

    Update Private

--- routes/private.js
+++ routes/private.js
@@ -4,13 +4,15 @@ import { PrismaClient } from '@prisma/client'
 const router = express.Router()
 const prisma = new PrismaClient()
 
-router.get('/listar-usuarios', async (req, res) => {
+router.get('/listar-usuarios', async (_req, res) => {
+
     try {
         const users = await prisma.user.findMany()
 
         res.status(200).json({message: 'Usuários listados com sucesso', users })
     } catch (err) {
         res.status(500).json({message: 'Falha no servidor' })
+        console.log(err)
     }
 })
 
