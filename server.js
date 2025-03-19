57bf704dde170d0eb54888f9de4c71f2257798b9
Leonardo Lopes <leobo215@hotmail.com>
2025-03-19 01:36:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,21 +3,25 @@ import publicRoutes from './routes/public.js'
 import privateRoutes from './routes/private.js'
 import fichaRoutes from './routes/CadastroFicha.js'
 import epiRoutes from './routes/CadastroEpi.js'
+import trocaRoutes from './routes/TrocarEpi.js'
 import cors from 'cors'
 import auth from './middlewares/auth.js'
 
-const app = express()
+
+export const app = express()
 app.use(cors())
 app.use(express.json())
 
 
 // 🔓 Rotas públicas
 app.use('/', publicRoutes)
+app.use('/', trocaRoutes)
 
 
-// 🔒 Rotas privadas protegidas
 app.use('/private', epiRoutes)
 app.use('/private', fichaRoutes)
+
+
 
 app.use('/private', auth, privateRoutes)
 
