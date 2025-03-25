6dcb32702ff60a01fb028c0b7fad2d73d645c2c5
Leonardo Lopes <leobo215@hotmail.com>
2025-03-25 02:31:17 -0300

    Update private.js

--- routes/private.js
+++ routes/private.js
@@ -8,7 +8,6 @@ router.get('/listar-usuarios', async (_req, res) => {
 
     try {
         const users = await prisma.user.findMany()
-
         res.status(200).json({message: 'Usuários listados com sucesso', users })
     } catch (err) {
         res.status(500).json({message: 'Falha no servidor' })
